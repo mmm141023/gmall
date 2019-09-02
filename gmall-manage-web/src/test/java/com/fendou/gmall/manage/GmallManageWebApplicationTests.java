9b078a17c3132ad55ef21df1e681f5faec8aad8d
mmm141023 <925670706@qq.com>
2019-09-02 18:32:34 +0800

    增加fastdfs测试

--- gmall-manage-web/src/test/java/com/fendou/gmall/manage/GmallManageWebApplicationTests.java
+++ gmall-manage-web/src/test/java/com/fendou/gmall/manage/GmallManageWebApplicationTests.java
@@ -1,16 +1,35 @@
 package com.fendou.gmall.manage;
 
+import org.csource.common.MyException;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallManageWebApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException, MyException {
+        String file = GmallManageWebApplicationTests.class.getResource("/tracker.conf").getPath();
+        ClientGlobal.init(file);
+        TrackerClient trackerClient = new TrackerClient();
+        TrackerServer trackerServer = trackerClient.getConnection();
+        StorageClient storageClient = new StorageClient(trackerServer, null);
+        String orginalFilename = "C://Users//Administrator//Desktop//phone.jpg";
+        String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
+        String url = "http://116.62.46.30";
+        for (int i = 0; i < upload_file.length; i++) {
+            url += "/" +  upload_file[i];
+            System.out.println(url);
+        }
     }
 
 }
