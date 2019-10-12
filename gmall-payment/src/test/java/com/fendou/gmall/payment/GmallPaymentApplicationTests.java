184941f10a2063d6f310d4f965f9aa34d4b944de
mmm141023 <925670706@qq.com>
2019-10-12 18:33:21 +0800

    引入activemq的配置文件

--- gmall-payment/src/test/java/com/fendou/gmall/payment/GmallPaymentApplicationTests.java
+++ gmall-payment/src/test/java/com/fendou/gmall/payment/GmallPaymentApplicationTests.java
@@ -1,16 +1,33 @@
 package com.fendou.gmall.payment;
 
+import com.fendou.gmall.mq.ActiveMQUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallPaymentApplicationTests {
 
+    @Autowired
+    ActiveMQUtil activeMQUtil;
+
     @Test
     public void contextLoads() {
+        ConnectionFactory connectionFactory = activeMQUtil.getConnectionFactory();
+        Connection connection = null;
+        try {
+            connection = connectionFactory.createConnection();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        System.out.println(connection);
     }
 
 }
